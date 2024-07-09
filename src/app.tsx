ff1133ff5bb96baa2eb9897d68d08558cc2ca3b6
Bruno Bento <brunobentoreinoso14@gmail.com>
2024-07-09 20:50:46 -0300

    finalizacao dia 01 journey

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X } from 'lucide-react'
-import { useState } from 'react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
+import { FormEvent, useState } from 'react'
 
 function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    'brunobento@gmail.com',
+    'bianca@gmail.com'
+  ])
 
   const openGuestsInput = () => {
     setIsGuestsInputOpen(true)
@@ -17,6 +21,30 @@ function App() {
   const closeGuestsModal = () => {
     setIsGuestsModalOpen(false)
   }
+
+  const addNewEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const data = new FormData(e.currentTarget) 
+    const email = data.get('email')?.toString();
+
+    if(!email)return;
+
+    if(emailsToInvite.includes(email)) return;
+
+    setEmailsToInvite([
+      ...emailsToInvite,
+      email
+    ])
+
+    e.currentTarget.reset()
+  }
+
+  const removeEmailFromInvites = (emailToRemove: string) => {
+    const newEmailList = emailsToInvite.filter(invited => invited !== emailToRemove);
+    setEmailsToInvite(newEmailList);
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -80,17 +108,48 @@ function App() {
       {
         isGuestsModalOpen && (
           <div className='fixed inset-0 bg-black/60  flex items-center justify-center'>
-            <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900'>
+            <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
               <div className='space-y-2'>
                 <div className='flex items-center justify-between'>
                   <h2 className='text-lg font-semibold'>Selecionar convidados</h2>
                   <button type='button' onClick={closeGuestsModal}>
-                    <X className='size-5 text-zinc-400' />
+                    <X className='size-5 text-zinc-400 hover:text-red-300' />
                   </button>
                 </div>
                 <p className='text-sm text-zinc-400'>
                   Os convidados irão receber e-mails para confirmar a participação na viagem.
                 </p>
+              </div>
+              <div className='flex flex-wrap gap-2'>
+                {
+                  emailsToInvite.map(email => {
+                    return (
+                      <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                        <span className='text-zinc-300'>{email}</span>
+                        <button type='button' onClick={() => removeEmailFromInvites(email)}>
+                          <X className='size-4 text-zinc-400 hover:text-red-300' />
+                        </button>
+                      </div>
+                    )
+                  })
+                }
+                <div className='w-full h-px bg-zinc-800' />
+
+                <form onSubmit={addNewEmail}  className='w-full p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+                  <div className='px-2 flex items-center flex-1 gap-2'>
+                    <AtSign className='text-zinc-400 size-5' />
+                    <input type="email"
+                      name='email'
+                      className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
+                      placeholder="Digite o e-mail do convidado"
+                    />
+                  </div>
+
+                  <button type="submit" className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                    Convidar
+                    <Plus className='text-lime-950 size-5' />
+                  </button>
+                </form>
               </div>
             </div>
           </div>
